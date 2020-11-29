2cfda2c3947f6874c92f722c7a571dd7efb86baf
André Sollander <andre@sollander.se>
2020-11-29 12:09:46 +0000

    Added pre-processing of the data and some restructuring

--- main.py
+++ main.py
@@ -1,25 +1,10 @@
 # ---------------- Import ----------------
-import numpy as np
-import tensorflow as tf
+from preprocessing import DataPreProcessing
+#import tensorflow as tf
+#import numpy as np
 
-# ---------------- Activation function ----------------
-# Sigmoid
-def Sigmoid(x):
-  y = np.inv(1 + np.exp(-x))
-  return y
+# ---------------- Main ----------------
+x = DataPreProcessing()
 
-# Relu
-def Relu(x):
-  y = np.maximum(0, x)
-  return y
-
-# ---------------- Cost function ----------------
-# Mean Square Error
-def MSE(yp, yt):
-  c = np.mean((yt-yp)^2)
-  return c
-
-# Cross Entropy
-def CE(yp, yt):
-  c = np.mean(-yt * np.log(yp) - (1 - yt) * np.log(1 - yp))
-  return c
+# Test
+print(x)
---
+++ preprocessing.py
@@ -0,0 +1,30 @@
+# ---------------- Import ----------------
+import numpy as np
+import csv
+
+def DataPreProcessing():
+  # ---------------- Variables ----------------
+  TimeVec = []
+  TempVec = []
+  HumiVec = []
+  PresVec = []
+  WindSpeVec = []
+  WindDirVec = []
+
+  # ---------------- Read csv ----------------
+  with open('Data/UTF8_2017_2018.csv', newline='') as csvfile:
+    dataFile = csv.DictReader(csvfile, delimiter='\t')
+
+    # Loop file
+    for row in dataFile:
+      TimeVec.append(row['Time'])
+      TempVec.append(float(row['Outdoor Temperature(°C)']))
+      HumiVec.append(float(row['Outdoor Humidity(%)']))
+      PresVec.append(float(row['Absolute Pressure(mmHg)']))
+      WindSpeVec.append(float(row['Wind Speed(m/s)']))
+      WindDirVec.append(row['Wind Direction'])
+
+  # ---------------- Operations ----------------
+
+  # Test
+  return(np.mean(TempVec))
