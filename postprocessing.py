965e95d2afd85a2c76e5b8ac60ba12af0c03139c
André Sollander <andre@sollander.se>
2020-11-29 13:57:07 +0000

    Calculates the number of days it rains

--- postprocessing.py
+++ postprocessing.py
@@ -14,8 +14,12 @@ def plotData(x, y):
 # Print stats 
 def printStats(x, y):
   # Print
-  print("The mean temperature is: ")
-  print(np.mean(x))
+  print("The mean temperature is: " + str(round(np.mean(x))))
+  print("The total amount of rain is: " + str(round(np.sum(x))))
 
-  print("The total amount of rain is: ")
-  print(np.sum(x))
+  # Rain calc
+  counter = 0
+  for days in y:
+    if days > 1:
+      counter += 1
+  print("It rains " + str(round((counter/np.size(y))*100)) + "% of the days.")
