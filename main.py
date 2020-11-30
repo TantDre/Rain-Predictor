dd32bdf7fc894f9e66acc1f0a3f4a160569f7c47
André Sollander <andre@sollander.se>
2020-11-30 13:44:07 +0000

    TensorFlow test

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 # ---------------- Import ----------------
+# Files
 from preprocessing import getData
 from postprocessing import plotData, printStats
-#import numpy as np
-#import tensorflow as tf
+
+# Libraries 
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
 
 # ---------------- Main ----------------
 # Pre-processing of data
 x, y, x_norm, y_bool = getData()
 
-# Print
-printStats(x, y)
+# Test
+def simpel_model(y_new):
+    x = np.array([0, 1, 2, 4, 6, 8, 10])
+    y = np.array([0.50, 1.00, 1.50, 2.50, 3.50, 4.50, 5.50])
+    model = tf.keras.Sequential([keras.layers.Dense(units = 1, input_shape = [1])])
+    model.compile(optimizer = 'sgd', loss = 'mean_squared_error')
+    model.fit(x, y, epochs= 100)
+    return model.predict(y_new)[0]
 
-# Plot data
-plotData(x, y)
+prediction =simpel_model([7.0])
+print(prediction)
