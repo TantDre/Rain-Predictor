220332de0b03c5c11fdee6b52b0dd6eb1c9938ff
Andre <andso041@student.liu.se>
2020-12-01 11:11:03 +0100

    Preprocessing for Tensorflow

--- main.py
+++ main.py
@@ -1,25 +1,15 @@
 # ---------------- Import ----------------
 # Files
 from preprocessing import getData
-from postprocessing import plotData, printStats
 
-# Libraries 
-import numpy as np
+# Libraries
+import matplotlib.pyplot as plt
 import tensorflow as tf
-from tensorflow import keras
+import numpy as np
+import pandas as pd
+import sys
+
 
 # ---------------- Main ----------------
 # Pre-processing of data
-x, y, x_norm, y_bool = getData()
-
-# Test
-def simpel_model(y_new):
-    x = np.array([0, 1, 2, 4, 6, 8, 10])
-    y = np.array([0.50, 1.00, 1.50, 2.50, 3.50, 4.50, 5.50])
-    model = tf.keras.Sequential([keras.layers.Dense(units = 1, input_shape = [1])])
-    model.compile(optimizer = 'sgd', loss = 'mean_squared_error')
-    model.fit(x, y, epochs= 100)
-    return model.predict(y_new)[0]
-
-prediction =simpel_model([7.0])
-print(prediction)
+x, y = getData()
--- preprocessing.py
+++ preprocessing.py
@@ -4,101 +4,116 @@ import csv
 
 # ---------------- getData() ----------------
 # Return the pre-processed data
+
+
 def getData():
-  # Get data
-  x, y = DataPreProcessing()
+    # Get data
+    x1, x2, x3, y = DataPreProcessing()
 
-  # Normalize data
-  x_norm = normalizeData(x)
+    # Normalize data
+    x1_norm = normalizeData(x1)
+    x2_norm = normalizeData(x2)
+    x3_norm = normalizeData(x3)
 
-  # Convert true value
-  y_bool = boolRain(y)
+    # Convert true value
+    y_bool = boolRain(y)
 
-  # Return
-  return x, y, x_norm, y_bool
+    # Format arrays
+    y_vec = np.array(y_bool, "float32")
+    x1_vec = np.array(x1_norm, "float32")
+    x2_vec = np.array(x2_norm, "float32")
+    x3_vec = np.array(x3_norm, "float32")
+    x_vec = np.stack((x1_vec, x2_vec, x3_vec), axis=0).T
+
+    # Return
+    return x_vec, y_vec
 
 # ---------------- DataPreProcessing() ----------------
 # Reads and reduces the raw data
+
+
 def DataPreProcessing():
-  # Variables
-  TimeVec = []
-  TempVec = []
-  HumiVec = []
-  PresVec = []
-  WindSpeVec = []
-  WindDirVec = []
-  RainVec = []
+    # Variables
+    TimeVec = []
+    TempVec = []
+    HumiVec = []
+    PresVec = []
+    WindSpeVec = []
+    WindDirVec = []
+    RainVec = []
 
-  # Read csv
-  with open('Data/Data_2017_2018.csv', newline='') as csvfile:
-    dataFile = csv.DictReader(csvfile, delimiter='\t')
+    # Read csv
+    with open('Data/Data_2017_2018.csv', newline='') as csvfile:
+        dataFile = csv.DictReader(csvfile, delimiter='\t')
 
-    # Loop file
-    for row in dataFile:
-      TimeVec.append(row['Time'])
-      TempVec.append(float(row['Outdoor Temperature(°C)']))
-      HumiVec.append(float(row['Outdoor Humidity(%)']))
-      PresVec.append(float(row['Absolute Pressure(mmHg)']))
-      WindSpeVec.append(float(row['Wind Speed(m/s)']))
-      WindDirVec.append(row['Wind Direction'])
-      RainVec.append(float(row['24 Hour Rainfall(mm)']))
+        # Loop file
+        for row in dataFile:
+            TimeVec.append(row['Time'])
+            TempVec.append(float(row['Outdoor Temperature(C)']))
+            HumiVec.append(float(row['Outdoor Humidity(%)']))
+            PresVec.append(float(row['Absolute Pressure(mmHg)']))
+            WindSpeVec.append(float(row['Wind Speed(m/s)']))
+            WindDirVec.append(row['Wind Direction'])
+            RainVec.append(float(row['24 Hour Rainfall(mm)']))
 
-  # Variables
-  day = int(TimeVec[0][9:11])
-  indexStart = 0
-  indexStop = 0
+    # Variables
+    day = int(TimeVec[0][9:11])
+    indexStart = 0
+    indexStop = 0
 
-  TimeVecRed = []
-  TempVecRed = []
-  HumiVecRed = []
-  PresVecRed = []
-  WindSpeVecRed = []
-  RainVecRed = []
+    TimeVecRed = []
+    TempVecRed = []
+    HumiVecRed = []
+    PresVecRed = []
+    RainVecRed = []
 
-  # Reduce data to days
-  for row in TimeVec:
-    if day != int(row[9:11]):
-      day = int(row[9:11])
-      TimeVecRed.append(day)
-      TempVecRed.append(np.mean(TempVec[indexStart:indexStop]))
-      HumiVecRed.append(np.mean(HumiVec[indexStart:indexStop]))
-      PresVecRed.append(np.mean(PresVec[indexStart:indexStop]))
-      WindSpeVecRed.append(np.mean(WindSpeVec[indexStart:indexStop]))
-      RainVecRed.append(np.sum(RainVec[indexStart:indexStop]))
-      indexStart = indexStop 
-    
-    else:
-      indexStop += 1
+    # Reduce data to days
+    for row in TimeVec:
+        if day != int(row[9:11]):
+            day = int(row[9:11])
+            TimeVecRed.append(day)
+            TempVecRed.append(np.mean(TempVec[indexStart:indexStop]))
+            HumiVecRed.append(np.mean(HumiVec[indexStart:indexStop]))
+            PresVecRed.append(np.mean(PresVec[indexStart:indexStop]))
+            RainVecRed.append(np.sum(RainVec[indexStart:indexStop]))
+            indexStart = indexStop
 
-  # Return
-  return TempVecRed, RainVecRed
+        else:
+            indexStop += 1
+
+    # Return
+    return TempVecRed, HumiVecRed, PresVecRed, RainVecRed
 
 # ---------------- normalizeData(X) ----------------
 # Normalizes the data
+
+
 def normalizeData(x):
-  # Variables
-  x_min = min(x)
-  x_max = max(x)
+    # Variables
+    x_min = min(x)
+    x_max = max(x)
 
-  # Normalize
-  x_norm = (x - x_min) / (x_max - x_min)
+    # Normalize
+    x_norm = (x - x_min) / (x_max - x_min)
 
-  # Return
-  return x_norm
+    # Return
+    return x_norm
 
 # ---------------- boolRain(x) ----------------
 # Converts mm rain to true or false
+
+
 def boolRain(y):
-  # Variables
-  rainLimit = 1
-  y_bool = []
+    # Variables
+    rainLimit = 1
+    y_bool = []
 
-  # Convert
-  for days in y:
-    if days > rainLimit:
-      y_bool.append(1)
-    else:
-      y_bool.append(0)
+    # Convert
+    for days in y:
+        if days > rainLimit:
+            y_bool.append(1)
+        else:
+            y_bool.append(0)
 
-  # Return
-  return y_bool
+    # Return
+    return y_bool
